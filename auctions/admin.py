8f4f753f6ff4f755b0a8f25ecbc526babbeeb86a
Will Norrell <willnorrell@Wills-MBP.lan>
2023-03-21 16:19:52 +0000

    Begun Create Listing

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, listings, Category
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(listings)
+admin.site.register(Category)
--- auctions/models.py
+++ auctions/models.py
@@ -5,7 +5,12 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+class Category(models.Model):
+    listingCategory = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.listingCategory
+    
 class listings(models.Model):
     '''
     origin = models.CharField(max_length=64)
@@ -15,5 +20,12 @@ class listings(models.Model):
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
     '''
-    owner = User
-    
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+    title = models.CharField(max_length=300)
+    description = models.CharField(max_length=3000)
+    active = models.BooleanField(default=True)
+    price = models.FloatField()
+    image = models.CharField(max_length=3000)
+    listingCategory = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category")
+
+
